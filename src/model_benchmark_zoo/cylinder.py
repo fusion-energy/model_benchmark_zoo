fdd852d42632ff82c399ddffbf2df401619e975c
Jonathan Shimwell <mail@jshimwell.com>
2023-11-21 23:06:37 +0000

    fixed typo

--- src/model_benchmark_zoo/cylinder.py
+++ src/model_benchmark_zoo/cylinder.py
@@ -7,7 +7,7 @@ class Cylinder:
     def csg_model(self):
         import openmc
 
-        surface_1 = =openmc.ZCylinder(x0=0.0, y0=0.0, r=self.radius)
+        surface_1 = openmc.ZCylinder(x0=0.0, y0=0.0, r=self.radius)
         surface_2 = openmc.ZPlane(z0=0.5*self.height)
         surface_3 = openmc.ZPlane(z0=-0.5*self.height)
     
