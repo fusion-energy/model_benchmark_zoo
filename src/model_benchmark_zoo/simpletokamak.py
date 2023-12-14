cbde2e3bad432a8bf22d8ae8554b06295e46a3da
Jonathan Shimwell <mail@jshimwell.com>
2023-12-14 22:23:24 +0000

    added blnkaet and center col

--- src/model_benchmark_zoo/simpletokamak.py
+++ src/model_benchmark_zoo/simpletokamak.py
@@ -1,41 +1,53 @@
 
-class NestedSphere:
-    def __init__(self, materials, radius1=10, radius2=5):
-        self.radius1 = radius1
-        self.radius2 = radius2
+class SimpleTokamak:
+    def __init__(self, materials, radius=500, blanket_thicknesses=100, center_column_thicknesses=50):
+        self.radius = radius
+        self.blanket_thicknesses = blanket_thicknesses
+        self.center_column_thicknesses = center_column_thicknesses
         self.materials = materials
 
     def csg_model(self):
         import openmc
         
-        surface1 = openmc.Sphere(r=self.radius1)
-        surface2 = openmc.Sphere(r=self.radius1+self.radius2, boundary_type="vacuum")
-        region1 = -surface1
-        region2 = +surface1 & -surface2
+        surface1 = openmc.Sphere(r=self.radius)
+        surface2 = openmc.Sphere(r=self.radius + self.blanket_thicknesses, boundary_type="vacuum")
+        surface3 = openmc.ZCylinder(r=self.center_column_thicknesses)
+        
+        region1 = -surface1 & +surface3  # plasma
+        region2 = +surface1 & -surface2  # blanket
+        region3 = -surface2 & -surface3  # center column
+
         cell1 = openmc.Cell(region=region1)
-        cell1.fill = self.materials[0]
         cell2 = openmc.Cell(region=region2)
         cell2.fill = self.materials[1]
-        geometry = openmc.Geometry([cell1, cell2])
+        cell3 = openmc.Cell(region=region3)
+        cell3.fill = self.materials[2]
+
+        geometry = openmc.Geometry([cell1, cell2, cell3])
         model = openmc.Model(geometry=geometry)
         return model
 
     def cadquery_assembly(self):
         import cadquery as cq
-        assembly = cq.Assembly(name="nestedshpere")
+        assembly = cq.Assembly(name="simpletokamak")
+
+        sphere_envelope = cq.Workplane().sphere(self.radius1 + self.blanket_thicknesses)
+
+        center_column = cq.Workplane("XY").circle(self.radius).extrude(self.radius+self.blanket_thicknesses).intersect(sphere_envelope)
+
         sphere1 = cq.Workplane().sphere(self.radius1)
-        sphere2 = cq.Workplane().sphere(self.radius1 + self.radius2).cut(sphere1)
+        sphere2 = cq.Workplane().sphere(self.radius1 + self.blanket_thicknesses).cut(sphere1).cut(center_column)
         assembly.add(sphere1)
         assembly.add(sphere2)
         return assembly
 
-    def export_stp_file(self, filename="sphere.step"):
+    def export_stp_file(self, filename="simpletokamak.step"):
         self.cadquery_assembly().save(filename, "STEP")
 
-    def dagmc_model(self, filename="nestedshpere.h5m", min_mesh_size=0.1, max_mesh_size=100.0):
+    def dagmc_model(self, filename="simpletokamak.h5m", min_mesh_size=0.1, max_mesh_size=100.0):
         from cad_to_dagmc import CadToDagmc
         import openmc
-        
+
         assembly = self.cadquery_assembly()
         ctd = CadToDagmc()
         material_tags = [self.materials[0].name, self.materials[1].name]
